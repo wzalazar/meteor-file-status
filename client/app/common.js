abf868e4699132dc6a273bb3a217f15285da05d7
Walter Zalazar <tecnicomdp.wz@gmail.com>
2015-01-29 15:31:10 -0300

    fix bug change diff to normal

--- client/app/common.js
+++ client/app/common.js
@@ -3,20 +3,28 @@ var codeMirror = {
 	initUI : function initUI(codeMirrorMerge) {
 			  if (codeMirrorMerge.value == null) return;
 			  if (document.getElementById(codeMirrorMerge.id)){
-			  var target = document.getElementById(codeMirrorMerge.id);
-			  target.innerHTML = "";
-			  this.value= CodeMirror.MergeView(target, {
-				    value: codeMirrorMerge.value,
-				    origLeft: codeMirrorMerge.panes == 3 && !codeMirrorMerge.collapse && !codeMirrorMerge.connect ? codeMirrorMerge.orig1 : null,
-				    orig: codeMirrorMerge.orig2,
-				    lineNumbers: true,
-				    mode: "text/html",
-				    highlightDifferences: codeMirrorMerge.highlight,
-				    connect: codeMirrorMerge.connect,
-				    collapseIdentical: codeMirrorMerge.collapse
-				  });
+				  var target = document.getElementById(codeMirrorMerge.id);
+				  target.innerHTML = "";
+				  if (codeMirrorMerge.orig2){
+			    	this.value= CodeMirror.MergeView(target, {
+					    value: codeMirrorMerge.value,
+					    origLeft: codeMirrorMerge.panes == 3 && !codeMirrorMerge.collapse && !codeMirrorMerge.connect ? codeMirrorMerge.orig1 : null,
+					    orig: codeMirrorMerge.orig2,
+					    lineNumbers: true,
+					    mode: "text/html",
+					    highlightDifferences: codeMirrorMerge.highlight,
+					    connect: codeMirrorMerge.connect,
+					    collapseIdentical: codeMirrorMerge.collapse
+				    });
+			      }
+			      else{
+		    		this.value= CodeMirror.MergeView(target, {
+				    	value: codeMirrorMerge.value,
+				    	lineNumbers: true
+			   		});
+			      }
 			   }
-			},
+	},
 
 	merge: function codeMirrorMerge(codeMirrorMerge){
 	       this.initUI(codeMirrorMerge);
